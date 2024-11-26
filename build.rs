35f4b981328bd6ac3f5f6b1d88dfe264fa8cf6e5
Michael Hupf <lpmichi38@gmail.com>
2024-11-26 23:56:36 +0000

    added icon for Windows build

---
+++ build.rs
@@ -0,0 +1,12 @@
+use std::{env, io};
+use winresource::WindowsResource;
+
+fn main() -> io::Result<()> {
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        WindowsResource::new()
+            // This path can be absolute, or relative to your crate root.
+            .set_icon("assets/icon.ico")
+            .compile()?;
+    }
+    Ok(())
+}

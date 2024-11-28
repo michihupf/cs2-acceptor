3c2b2c353646f86bebc0b2bb67d843ea2226f2cf
michihupf <michihupf@protonmail.com>
2024-11-29 00:53:25 +0100

    added clippy and ignore-cs2 feature for testing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+
+use image::Pixel;
+use rdev::{listen, simulate, Event, EventType, Key, SimulateError};
 use std::{
-    time,
     process::exit,
     thread::{self, sleep},
     time::Duration,
 };
+use xcap::Monitor;
 
-use image::Pixel;
-use rdev::{listen, simulate, Event, EventType, Key, SimulateError};
-use xcap::{Monitor, Window};
+#[cfg(not(feature = "ignore-cs2"))]
+use xcap::Window;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -16,28 +19,39 @@ const BUTTON_COLOR: [u8; 3] = [55, 182, 82];
 const HOVER_COLOR: [u8; 3] = [58, 198, 89];
 
 fn main() {
-    println!("CS2 Match Acceptor v{}", VERSION);
+    println!("CS2 Match Acceptor v{VERSION}");
     println!("Setting up failsafe key [B].");
     thread::spawn(|| {
         if let Err(e) = listen(callback) {
-            println!("Error: {:?}", e);
+            println!("Error: {e:?}");
         }
     });
-    
-    println!("Looking for CS2...");
-    let mut cs2 = loop {
-        // title should be same on all platforms
-        if let Some(cs2) = Window::all().unwrap().into_iter().find(|w| w.title() == "Counter-Strike 2") {
-            break cs2;
-        }
-        sleep(Duration::from_secs(2));
-    };
+
+    #[cfg(not(feature = "ignore-cs2"))]
+    {
+        println!("Looking for CS2...");
+        let mut cs2 = loop {
+            // title should be same on all platforms
+            if let Some(cs2) = Window::all()
+                .unwrap()
+                .into_iter()
+                .find(|w| w.title() == "Counter-Strike 2")
+            {
+                break cs2;
+            }
+            sleep(Duration::from_secs(2));
+        };
+    }
 
     // On Windows monitors are positioned relative to the primary screen so clicking needs to be offset.
     let monitors = Monitor::all().unwrap();
-    let offset_x = -&monitors.iter().map(|m| m.x()).min().unwrap();
-    let offset_y = -&monitors.iter().map(|m| m.y()).min().unwrap();
-    
+    let offset_x = -&monitors.iter().map(xcap::Monitor::x).min().unwrap();
+    let offset_y = -&monitors.iter().map(xcap::Monitor::y).min().unwrap();
+
+    #[cfg(feature = "ignore-cs2")]
+    let monitor = monitors.iter().find(|m| m.is_primary()).unwrap();
+
+    #[cfg(not(feature = "ignore-cs2"))]
     let monitor = cs2.current_monitor();
     println!(
         "Found CS2 on monitor {}. x,y: ({}, {})",
@@ -45,25 +59,28 @@ fn main() {
         monitor.x() + offset_x,
         monitor.y() + offset_y
     );
-     
+
     // update offsets for clicking later
-    let offset_x = (monitor.x() + offset_x + 10) as f64;
-    let offset_y = (monitor.y() + offset_y + 10) as f64;
+    let offset_x = f64::from(monitor.x() + offset_x + 10);
+    let offset_y = f64::from(monitor.y() + offset_y + 10);
 
     println!("Acceptor is ready!");
 
     loop {
-        if let Err(err) = cs2.refresh() {
-            // user probably closed CS2
-            println!("ERROR: {}", err);
-            exit(0);
+        #[cfg(not(feature = "ignore-cs2"))]
+        {
+            if let Err(err) = cs2.refresh() {
+                // user probably closed CS2
+                println!("ERROR: {err}");
+                exit(0);
+            }
+            if cs2.is_minimized() {
+                println!("CS2 is minimized. Skipping screengrab.");
+                sleep(Duration::from_secs(2));
+                continue;
+            }
         }
-        if cs2.is_minimized() {
-            println!("CS2 is minimized. Skipping screengrab.");
-            sleep(Duration::from_secs(2));
-            continue;
-        }
-        
+
         // capture screen and find button
         let image = monitor.capture_image().unwrap();
         let mut same_count = 0;
@@ -78,11 +95,11 @@ fn main() {
 
                 if same_count == 10 {
                     // click on the button
-                    println!("I found the button! x,y: ({}, {})", x, y);
+                    println!("I found the button! x,y: ({x}, {y})");
 
                     send(&EventType::MouseMove {
-                        x: x as f64 + offset_x,
-                        y: y as f64 + offset_y,
+                        x: f64::from(x) + offset_x,
+                        y: f64::from(y) + offset_y,
                     });
                     send(&EventType::ButtonPress(rdev::Button::Left));
                     send(&EventType::ButtonRelease(rdev::Button::Left));
@@ -96,11 +113,11 @@ fn main() {
 }
 
 fn send(event_type: &EventType) {
-    let delay = time::Duration::from_millis(30);
+    let delay = Duration::from_millis(30);
     match simulate(event_type) {
         Ok(()) => (),
         Err(SimulateError) => {
-            println!("We could not send {:?}", event_type);
+            println!("We could not send {event_type:?}");
         }
     }
 
@@ -108,13 +125,15 @@ fn send(event_type: &EventType) {
     thread::sleep(delay);
 }
 
-fn is_same_color(c1: &[u8], c2: &[u8]) -> bool {
+const fn is_same_color(c1: &[u8], c2: &[u8]) -> bool {
     // maybe this is better? c1.iter().zip(c2.iter()).all(|x| x[0].abs_diff(x[1]) <= 2)
     c1[0].abs_diff(c2[0]) <= 2 && c1[1].abs_diff(c2[1]) <= 2 && c1[2].abs_diff(c2[2]) <= 2
 }
 
+// ALLOW: Interface is given by rdev::listen
+#[allow(clippy::needless_pass_by_value)]
 fn callback(event: Event) {
-    if let EventType::KeyPress(Key::KeyB) = event.event_type {
+    if event.event_type == EventType::KeyPress(Key::KeyB) {
         println!("Nuking process!");
         exit(0);
     }
